a36eee7978a55192105c932b462cf000839819b0
Juuso Issakainen <7767433+juusoi@users.noreply.github.com>
2023-03-31 01:37:48 +0300

    ci

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'arpa-api',
+      script: './dist/index.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
